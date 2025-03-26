98e23e40f1bdf6ce7b552467b1c32642c83666bb
Ndife Somkenechukwu <ndife.somkenechukwu@gmail.com>
2025-03-26 17:47:09 +0100

    Updated pages

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-import express from 'express';
-import {workTimevalidate} from "./middleware/workTimeValidation.js"
+import express from "express";
+import { workTimevalidate } from "./middleware/workTimeValidation.js";
 
 const app = express();
 const port = 3000;
+app.listen(port);
 
-app.use(workTimevalidate)
-app.set("view engine", "pug");
-app.set("views", "./views")
+app.use(workTimevalidate);
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.get("/", (req,res)=>{
-    res.render('index');
+app.use(express.static("public"));
+app.use(express.static("res"));
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/services", (req,res)=>{
-    res.render('services');
+app.get("/services", (req, res) => {
+  res.render("services");
 });
 
-app.get("/contact", (req,res)=>{
-    res.render('contact');
-})
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("*", (req, res) => {
+  res.render("notFound");
+});
